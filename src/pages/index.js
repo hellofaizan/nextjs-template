445b950825ec9b9eaf0c1607905604d412f4920b
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-09 22:01:19 +0530

    Added Tailwinds CSS also

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <div>
-      <p>
+      <p className="text-3xl font-bold underline">
         Hello World
       </p>
     </div>
