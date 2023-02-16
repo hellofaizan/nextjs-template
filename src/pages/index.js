7328af7778a24ac91407e99ad90247a0787697d5
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-16 21:40:20 +0530

    Using useEffect to get data from API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Inter } from '@next/font/google'
+import { useState, useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {  
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
-export const getStaticProps = async () => {
-  const res = await fetch('YOUR_API_URL')
-  const data = await res.json()
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://api.npoint.io/4a8d62649d30ab3f091e', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        console.log(data);
+      });
+  }, []);
 
-  return {
-    props: { data },  
-  }
-}
-
-const Home = ({data}) => {  
-  console.log(data)
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
   
   return (
     <>
