f8837b6c7b44b9e5298a4d213fda237b303f2260
Mohammad Faizan <faizancurious@gmail.com>
2023-03-04 22:32:20 +0530

    Added Axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,28 +5,17 @@ import axios from 'axios'
 
 const Home = () => {  
   const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
     axios.get('https://api.npoint.io/4a8d62649d30ab3f091e', {
       method: 'GET',
     })
       .then((res) => res.json())
       .then((data) => {
         setData(data);
-        setLoading(false);
         console.log(data);
       });
   }, [data]);
-
-  if (isLoading) return <div className='w-screen h-screen flex justify-center items-center'>
-    <Image src={"/faizan.png"} width={60} height={60} alt="HelloFaizan Splach Screen Logo"></Image>
-  </div>
-
-  if (!data) return <div className='w-screen h-screen flex justify-center items-center'>
-    <Image src={"/faizan.png"} width={60} height={60} alt="HelloFaizan Splach Screen Logo"></Image>
-  </div>
 
   return (
     <>
